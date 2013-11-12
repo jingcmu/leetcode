c1d30f5017e32b960ecad8c0af21a5d11fb62528
Jacky Chen <jingchen@cmu.edu>
2013-11-12 10:24:51 -0800

    Single Number

---
+++ Single Number.cpp
@@ -0,0 +1,12 @@
+//using exclusive or to remove all bits which appear even times.
+class Solution {
+public:
+   int singleNumber(int A[], int n) {
+// Note: The Solution object is instantiated only once and is reused by each test case.
+   int res = 0;
+   for(int i=0; i<n; i++){
+       res ^= A[i];
+   }
+   return res;
+   }
+};
