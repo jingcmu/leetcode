542bcee4bdb514d70166b1de5a42771595b2c577
Jacky Chen <jingchen@cmu.edu>
2013-12-13 16:26:27 -0800

    Update word break

--- Word Break.cpp
+++ Word Break.cpp
@@ -1,3 +1,4 @@
+//DFS解法
 class Solution {
 private:
     map<int, bool> m;
@@ -20,4 +21,26 @@ public:
         }
         return false;
     }
+};
+
+//DP解法
+/*设状态为f(i)，表示s[0,i] 是否可以分词，则状态转移方程为
+f(i) = any_of(f(j)&&s[j + 1, i] 在 dict 中); 0 <= j < i
+*/
+class Solution {
+public:
+	bool wordBreak(string s, unordered_set<string> &dict) {
+		// 长度为n 的字符串有n+1 个隔板
+		vector<bool> f(s.size() + 1, false);
+		f[0] = true; // 空字符串
+		for (int i = 1; i <= s.size(); ++i) {
+			for (int j = i - 1; j >= 0; --j) {
+				if (f[j] && dict.find(s.substr(j, i - j)) != dict.end()) {
+					f[i] = true;
+					break;
+				}
+			}
+		}
+		return f[s.size()];
+	}
 };
