e6bff823ab67833d932f465c1397ed10a5e693d6
Jacky Chen <jingchen@cmu.edu>
2014-01-02 10:35:35 -0800

    Valid Palindrome.cpp

---
+++ Valid Palindrome.cpp
@@ -0,0 +1,46 @@
+My solution:
+双指针的思想真是无处不在啊，两头往中间遍历貌似可以ko掉很多题
+不过这一题比USACO上的类似的一道题简单
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        int len = s.length();
+        bool isPalin = true;
+        int head = 0, tail = len-1;
+        if(!len){
+            return true;
+        }
+        while(head<tail){
+            if(!(isalpha(s[head]) || isdigit(s[head]))){
+                head++;
+                continue;
+            }
+            if(!(isalpha(s[tail]) || isdigit(s[tail]))){
+                tail--;
+                continue;
+            }
+            if(s[head] != s[tail] && abs(s[head]-s[tail]) != 32){
+                return false;
+            }
+            head++;
+            tail--;
+        }
+        return isPalin;
+    }
+};
+
+纯STL实现方法
+class Solution {
+public:
+	bool isPalindrome(string s) {
+		transform(s.begin(), s.end(), s.begin(), ::tolower);
+		auto left = s.begin(), right = prev(s.end());
+		while (left < right) {
+			if (!::isalnum(*left)) ++left;
+			else if (!::isalnum(*right)) --right;
+			else if (*left != *right) return false;
+		}
+		return true;
+	}
+};
