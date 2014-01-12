89bee8b03ee7846372d990c554e32d7b550b9f1d
Jacky Chen <jingchen@cmu.edu>
2014-01-12 15:11:07 -0800

    Trapping Rain Water.cpp

--- Trapping Rain Water.cpp
+++ Trapping Rain Water.cpp
@@ -1,7 +1,18 @@
+/*
+两种解法：
+对于每个柱子，找到其左右两边最高的柱子，该柱子能容纳的水就是
+min(max_left,max_right) - height。所以，
+1. 从左往右扫描一遍，对于每个柱子，求取左边最大值；
+2. 从右往左扫描一遍，对于每个柱子，求最大右值；
+3. 再扫描一遍，把每个柱子能容纳的水累加起来就行了
+也可以，
+1. 扫描一遍，找到最高的柱子，这个柱子将数组分为两半；
+2. 处理左边一半；
+3. 处理右边一半。
+*/
 class Solution {
 public:
     int trap(int A[], int n) {
-        // Note: The Solution object is instantiated only once and is reused by each test case.
         int maxIdx=0, max=A[0], max_bar, rain = 0;
         //获取最长板的位置
         for(int i=1; i<n; i++){
--- Valid Sudoku.cpp
+++ Valid Sudoku.cpp
@@ -1,3 +1,7 @@
+/*
+第一种解法：
+对每个放置的元素检查横竖框
+*/
 class Solution {
 public:
     bool isValid(vector<vector<char> > &board, int x, int y, int len){
@@ -25,7 +29,6 @@ public:
         return true;
     }
     bool isValidSudoku(vector<vector<char> > &board) {
-        // Note: The Solution object is instantiated only once and is reused by each test case.
         int len = board.size();
         for(int i=0; i<len; i++){
             for(int j=0; j<len; j++){
@@ -36,4 +39,42 @@ public:
         }
         return true;
     }
+};
+
+/*
+第二种解法：
+对每个横竖框检查里面的元素，显然这种方法效率高
+*/
+class Solution {
+public:
+	bool isValidSudoku(vector<vector<char>>& board) {
+		bool used[9];
+		for (int i = 0; i < 9; ++i) {
+		fill(used, used + 9, false); 
+		for (int j = 0; j < 9; ++j) // 检查行
+			if (!check(board[i][j], used))
+				return false;
+		fill(used, used + 9, false);
+		for (int j = 0; j < 9; ++j) // 检查列
+			if (!check(board[j][i], used))
+				return false;
+		}
+		for (int r = 0; r < 3; ++r) // 检查9 个子格子
+		for (int c = 0; c < 3; ++c) {
+			fill(used, used + 9, false);
+			for (int i = r * 3; i < r * 3 + 3; ++i)
+			for (int j = c * 3; j < c * 3 + 3; ++j)
+			if (!check(board[i][j], used))
+			return false;
+		}
+		return true;
+	}
+	
+	//检查横竖或框中的一个位置，顺便把已经出现过的数字标记下
+	bool check(char ch, bool used[9]) {
+		if (ch == '.') return true;
+		if (used[ch - '1']) return false;
+		used[ch - '1'] = true;
+		return true;
+	}
 };
