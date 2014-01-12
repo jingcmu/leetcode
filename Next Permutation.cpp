84ef76b87b78e12a695dfd72e3b8a886ca2792a0
Jacky Chen <jingchen@cmu.edu>
2014-01-12 11:25:02 -0800

    Next Permutation.cpp

--- Next Permutation.cpp
+++ Next Permutation.cpp
@@ -9,24 +9,25 @@ public:
     void nextPermutation(vector<int> &num) {
         // Note: The Solution object is instantiated only once and is reused by each test case.
         int len = num.size(), index = -1;
-        //从后往前找出现的第一个升序排列的数对（后一个数大的数对）
+        //从后往前找出现的第一个升序排列的数对
         for(int i=len-2; i>=0; i--){
             if(num[i] < num[i+1]){
                 index = i;//记录下较小数的位置
                 break;
             }
         }
-        if(index == -1){
+        if(index == -1){ //找不到升序排列的数对,i < ii
             sort(num.begin(),num.end());
             return;
         }
-        //在顺序数对后找最小的那个大于较小数的数，跟较小数交换
+        //在顺序数对后，从最右边找一个大于i的数，跟i交换
         for(int i=len-1; i>index; i--){
-            if(num[i]>num[index]){
+            if(num[i] > num[index]){
                 swap(num, index, i);
-                break;              //记得要break，这里WA了一次
+                break;      //记得要break，这里WA了一次
             }
         }
+		//然后，从ii开始往右排序，就得到next_permutation了
         sort(num.begin()+index+1, num.end());
     }
 };
