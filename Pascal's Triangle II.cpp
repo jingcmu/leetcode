ab8d84546b57de4f3a8bf38e6922491e49ad389a
Jacky Chen <jingchen@cmu.edu>
2014-01-17 16:28:06 -0800

    update Pascal's Triangle II.cpp

--- Pascal's Triangle II.cpp
+++ Pascal's Triangle II.cpp
@@ -1,18 +1,20 @@
+/*
+只需要两个vector即可，
+用v来进行计算，用v_last来存上一行的
+rowIndex是从0th开始算的
+*/
 class Solution {
 public:
      vector<int> getRow( int rowIndex) {
-        // Note: The Solution object is instantiated only once and is reused by each test case.
         vector< int> v, v_last;
-        rowIndex++;
+        rowIndex++; //先加1，从1开始算，这样行数和元素数相等
         v.push_back(1);
         v_last = v;
-        if(rowIndex == 1){
-            return v;
-        }
-        for( int i=2; i<=rowIndex; i++){
+        if(rowIndex == 1) return v;
+        for( int i=2; i<=rowIndex; i++){ //从第2行到第rowIndex行
             v.clear();
             v.push_back(1);
-            for( int j=0; j<i-2; j++){
+            for( int j=0; j<i-2; j++) { //中间有i-2个元素
                 v.push_back(v_last[j]+v_last[j+1]);
             }
             v.push_back(1);
@@ -20,5 +22,4 @@ public:
         }
         return v;
     }
-
 };
