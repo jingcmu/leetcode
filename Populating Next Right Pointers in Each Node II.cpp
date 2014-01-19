0f8245cde2fe7b17a0d5e833f7b60e645bdc219f
Jacky Chen <jingchen@cmu.edu>
2014-01-19 13:31:16 -0800

    update Populating Next Right Pointers in Each Node II.cpp

--- Populating Next Right Pointers in Each Node II.cpp
+++ Populating Next Right Pointers in Each Node II.cpp
@@ -6,37 +6,36 @@
  *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
  * };
  */
+//思路：用vector模拟queue操作，把每一层的连起来即可
 class Solution {
 public:
     void levelOrder(TreeLinkNode *root) {
-        // Note: The Solution object is instantiated only once and is reused by each test case.
-        vector<TreeLinkNode*> tree;
+        vector<TreeLinkNode*> tree; //用vector模拟队列操作
         int head = 0, tail = 0;
-        if(root){
+        if(root){ //如果root不是空，先push进去
             tree.push_back(root);
             tail++;
         }
         while(head!=tail){
-            int tail_current = tail;
+            int tail_current = tail; //记录下当前的tail位置，从head到tail_current是一层
             for(; head<tail_current; head++){
-                if(tree[head]->left){
+                if(tree[head]->left){ //先左
                     tree.push_back(tree[head]->left);
                     tail++;
                 }
-                if(tree[head]->right){
+                if(tree[head]->right){ //后右
                     tree.push_back(tree[head]->right);
                     tail++;
                 }
             }
+			//把刚才push进去的节点连起来
             for(int i=head; i<tail-1; i++){
                 tree[i]->next = tree[i+1];
             }
         }
     }
 
-
     void connect(TreeLinkNode *root) {
-        // Note: The Solution object is instantiated only once and is reused by each test case.
         levelOrder(root);
     }
 };
