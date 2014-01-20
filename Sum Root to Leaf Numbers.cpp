6f164342fe1cb6ab332c80b62ee62324ec4d92a1
Jacky Chen <jingchen@cmu.edu>
2014-01-19 23:06:17 -0800

    Sum Root to Leaf Numbers.cpp

--- Sum Root to Leaf Numbers.cpp
+++ Sum Root to Leaf Numbers.cpp
@@ -7,29 +7,30 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
+/*
+采用DFS，递归的时候，传下去当前节点的值
+*/
 class Solution {
 public:
     int sumNumbers(TreeNode *tree, int number){
         int sum = number*10, left=0, right=0;
-        sum += tree->val;
-        if(tree->left){
+        sum += tree->val; //得到当前节点的值
+        if(tree->left) { //如果左边不为空，递归求解左子树
             left = sumNumbers(tree->left, sum);
         }
-        if(tree->right){
+        if(tree->right) {//如果右边不为空，递归求解右子树
             right = sumNumbers(tree->right, sum);
         }
-        if(!left && !right){
+        if(!left && !right) { //如果左右都为空，返回当前节点的值
             return sum;
         }
-        else{
-            return left+right;
+        else {
+            return left+right; //否则，返回两边的和
         }
     }
     int sumNumbers(TreeNode *root) {
-        // Note: The Solution object is instantiated only once and is reused by each test case.
-        if(root == NULL){
-            return 0;
-        }
+        if(root == NULL) return 0;
         return sumNumbers(root, 0);
     }
 };
