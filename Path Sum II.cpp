e807e23f87dbf91dd439c644701001b39d2a137d
Jacky Chen <jingchen@cmu.edu>
2014-01-20 22:40:59 -0800

    .\Path Sum II.cpp

--- Path Sum II.cpp
+++ Path Sum II.cpp
@@ -7,34 +7,32 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+/*
+典型的DFS，没啥好说的
+*/
 class Solution {
 private:
     vector<vector<int> > ret;
-    int sum;
 public:
-    void pathSum(TreeNode *node, int sum2now, vector<int> temp){
-        if(!node){
-            return;
-        }
+    void pathSum(TreeNode *node, int sum2now, vector<int> temp, int sum){
+        if(!node) return;
         sum2now += node->val;
         temp.push_back(node->val);
         if(!node->left && !node->right){
-            if(sum2now == this->sum){
+            if(sum2now == sum){
                 ret.push_back(temp);
                 return;
             }
         }
         else{
-            pathSum(node->left, sum2now, temp);
-            pathSum(node->right, sum2now, temp);
+            pathSum(node->left, sum2now, temp, sum);
+            pathSum(node->right, sum2now, temp, sum);
         }
     }
     vector<vector<int> > pathSum(TreeNode *root, int sum) {
-        // Note: The Solution object is instantiated only once and is reused by each test case.
         vector<int> temp;
         ret.clear();
-        this->sum = sum;
-        pathSum(root, 0, temp);
+        pathSum(root, 0, temp, sum);
         return ret;
     }
 };
