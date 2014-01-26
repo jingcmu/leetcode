a3d1097b0e06bc6fb900ae269d11d61249c8b51f
Jacky Chen <jingchen@cmu.edu>
2014-01-25 21:34:49 -0800

    .\First Missing Positive.cpp

--- First Missing Positive.cpp
+++ First Missing Positive.cpp
@@ -1,8 +1,14 @@
+/*
+思路：
+大于n或者小于0的不可能占据正数的位置，所以直接pass
+如果遇到A[i] == i+1，说明这个数A[i]正好在它应该在的位置，pass
+如果遇到A[i] == A[A[i]-1]，说明不需要交换，pass
+如果不是上面的情况，则交换A[i] 和 A[A[i]-1]，就是把A[i]放到它应该在的位置
+对所有数进行了以上操作之后，再从头遍历一遍，返回第一个放错数的位置
+*/
 class Solution {
 public :
     int firstMissingPositive( int A[], int n) {
-        // IMPORTANT: Please reset any member data you declared, as
-        // the same Solution instance will be reused for each test case.
         int i = 0;
         while(i < n){
             if(A[i] == i+1 || A[i] <= 0 || A[i] > n || A[i] == A[A[i]-1]){
