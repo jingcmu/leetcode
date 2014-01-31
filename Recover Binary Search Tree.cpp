c6e9cca9fccad9743e5b62d3fe9d46cf8e97b490
Jacky Chen <jingchen@cmu.edu>
2014-01-31 11:09:24 -0800

    .\Recover Binary Search Tree.cpp

--- Recover Binary Search Tree.cpp
+++ Recover Binary Search Tree.cpp
@@ -7,33 +7,41 @@
 *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 * };
 */
+
+/*
+O(n) 空间的解法是，开一个指针数组，中序遍历，将节点指针依次存放到数组里，然后寻找两
+处逆向的位置，先从前往后找第一个逆序的位置，然后从后往前找第二个逆序的位置，交换这两个
+指针的值。
+*/
+
+/*
+O(log n) 空间的解法如下：
+用递归中序遍历，中序遍历的特点是元素访问是按照从小到大被访问的，如果碰到一个逆序的，
+则这个逆序的两个点有一个肯定是错了的，那么到底是哪个呢？
+从中序遍历的特点可知，第一个错误的节点肯定是逆序对中的较大的，第二个则是逆序对中较小的
+*/
 class Solution { 
 public : 
-    TreeNode *s1,*s2,*pre; 
-    void inorder(TreeNode *root) 
-    { 
-        if(!root){
-            return;
-        }
-        inorder(root->left); 
-        if(pre && pre->val > root->val) 
-        { 
-			//tricky here
-            if(s1 == NULL){  //the first wrong node is a larger one
-                s1 = pre;    //the second wrong node is a smaller one
-                s2 = root;
-            }
-            else s2 = root; 
+    TreeNode *first, *second, *pre; 
+    void inorder(TreeNode *root) {  
+		if(root->left) inorder(root->left);
+		if(pre == NULL) pre = root;
+		else if(pre->val > root->val) {
+            if(first == NULL)   //如果first还没被赋值，则将大的那个赋给first
+                first = pre;    
+            second = root; //如果first已经赋值过了，则将小的那个赋给second
         } 
         pre = root; 
-        inorder(root->right); 
+        if(root->right) inorder(root->right); 
     } 
     void recoverTree(TreeNode *root) { 
-        if(!root){
-            return;
-        }
-        s1 = s2 = pre = NULL; 
-        inorder(root); 
-        swap(s1->val,s2->val); 
-    } 
+        if(!root) return;
+        first = second = pre = NULL; 
+        inorder(root);
+        swap(first->val, second->val); //交换两个错误节点
+    }
 };  
+
+/*
+O(1) 空间的解法：Morris 中序遍历
+*/
