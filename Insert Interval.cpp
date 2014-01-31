25da7af1e18d4fe87e12d678059a96671859fac5
Jacky Chen <jingchen@cmu.edu>
2014-01-31 11:34:14 -0800

    .\Insert Interval.cpp

--- Insert Interval.cpp
+++ Insert Interval.cpp
@@ -10,29 +10,33 @@
 class Solution {
 public :
     vector<Interval> insert(vector<Interval> &intervals, Interval newInterval) {
-        vector<Interval> res;
-        bool merged = false;
+        vector<Interval> res; //开始是空的
+        bool merged = false; //标示是否已经成功merge
         int size = intervals.size();
-        for( int i=0; i<size; i++){
+        for( int i=0; i<size; i++) {
+			//newInterval.start在intervals[i].end之前，就代表需要merge了，
             if(!merged && newInterval.start <= intervals[i].end){
+				//update新Interval的起点
                 newInterval.start = newInterval.start < intervals[i].start? newInterval.start : intervals[i].start;
-                if(newInterval.end >= intervals[i].start){
-                    //merge newInterval with intervals[i]
+				//newInterval.end 在 intervals[i].start之后，则把新interval变大
+                if(newInterval.end >= intervals[i].start) {
+                    //update新Interval的终点
                     newInterval.end = newInterval.end > intervals[i].end? newInterval.end : intervals[i].end;
-                    if(i == size-1){
+                    if(i == size-1) { //如果后面没有interval了
                         merged = true;
                         res.push_back(newInterval);
+						break；
                     }
                     continue;
                 }
-                else{
+                else { //如果newInterval.end 在 intervals[i].start之前，则插入newInterval，标记为已merge
                     merged = true; //marked as "have merged"
                     res.push_back(newInterval);
                 }
             }
-            res.push_back(intervals[i]);
+            res.push_back(intervals[i]); //对于没有merge的或者在新interval之前的老intervals，都push到res中
         }
-        if(!merged){
+        if(!merged) {
             res.push_back(newInterval);
         }
         return res;
