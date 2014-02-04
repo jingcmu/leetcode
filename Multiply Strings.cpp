2190771a393d58df8190f994795b483ba515da59
Jacky Chen <jingchen@cmu.edu>
2014-02-04 13:52:48 -0800

    .\Multiply Strings.cpp

--- Multiply Strings.cpp
+++ Multiply Strings.cpp
@@ -1,20 +1,28 @@
+/*
+采用字符串操作：
+string multiply(string num1, string num2) 完成两个字符串相乘，调用如下两个函数：
+string multiply(string num, char c) 和 string add(string num1, string num2)
+分别完成一个字符串和一个char相乘 以及 两个字符串相加，
+这些都是基本的。
+*/
 class Solution {
 public:
-    string add(string num1, string num2){
+	//模拟加法操作，一位一位加，最后不要忘了判断进位
+    string add(string num1, string num2) {
         int len_a = num1.length(), temp;
         int len_b = num2.length();
         int carry = 0;
         string res = "";
-        if(len_a<len_b){
+        if(len_a < len_b) { //保证第一个字符串更长
             return add(num2, num1);
         }
-        for( int i=0; i<len_b; i++){
+        for(int i=0; i<len_b; i++) { 
             temp = (num1[i]-'0')+(num2[i]-'0')+carry;
             carry = temp/10;
             temp%=10;
             res += (temp+'0');
         }
-        for( int i=len_b; i<len_a; i++){
+        for(int i=len_b; i<len_a; i++) {
             temp = (num1[i]-'0')+carry;
             carry = temp/10;
             temp%=10;
@@ -26,7 +34,8 @@ public:
         return res;
     }
     
-    string multiply(string num, char c){
+	//比上面的加法更简单
+    string multiply(string num, char c) {
         int len = num.length(), carry = 0, temp;
         string res = "";
         for(int i=0; i<len; i++){
@@ -40,9 +49,8 @@ public:
         }
         return res;
     }
-    
+    //通过调用上面两个函数实现
     string multiply(string num1, string num2) {
-        // Note: The Solution object is instantiated only once and is reused by each test case.
         int len_a = num1.length(), temp;
         int len_b = num2.length();
         int carry = 0;
@@ -50,6 +58,7 @@ public:
         if(len_a<len_b){
             return multiply(num2, num1);
         }
+		//把数的顺序反过来，便于操作
         reverse(num1.begin(), num1.end());
         reverse(num2.begin(), num2.end());
         for(int i=0; i<len_b; i++){
