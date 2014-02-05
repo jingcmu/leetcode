87f2dcbcc21104240b60c96af2a6efe503057657
Jacky Chen <jingchen@cmu.edu>
2014-02-05 11:02:35 -0800

    .\Max Points on a Line.cpp

--- Max Points on a Line.cpp
+++ Max Points on a Line.cpp
@@ -7,39 +7,44 @@
  *     Point(int a, int b) : x(a), y(b) {}
  * };
  */
+/*
+思路：
+以每个点为中心，计算其他点与之连线的斜率，斜率相同的就在一条线上
+注意重合点的处理，数据结构使用unordered_map便于查找，以每个点为中心
+算出来之后都要查找所有其他点到它的斜率，查找频繁！
+只需要算当前点之后的点与之的连线斜率，之前的点不需要算
+*/
 class Solution {
 public:
     int maxPoints(vector<Point> &points) {
-        // IMPORTANT: Please reset any member data you declared, as
-        // the same Solution instance will be reused for each test case.
         int size = points.size();
-        unordered_map<float, int> mp;
-        int max = 0;
-        for(int i=0; i<size; i++){
+        unordered_map<float, int> mp; //斜率 点数
+        int max_point = 0;
+        for(int i=0; i<size; i++) {
             int duplicate = 1;
-            mp.clear();       //防止重复计算的情况  
-            mp[INT_MIN] = 0;  //防止出现只有一个点的情况
-            for(int j = i+1; j<size; j++){
+            mp.clear();
+            mp[INT_MIN] = 0;
+            for(int j = i+1; j<size; j++) {
                 float k;
-                //计算重合点数
-                if(points[i].x == points[j].x && points[i].y == points[j].y){
+                //计算重合点数(有可能有重)
+                if(points[i].x == points[j].x && points[i].y == points[j].y) {
                     duplicate++;
-                    continue;
+                    continue; //重合点不必算斜率
                 }
                 //计算斜率
-                if(points[i].x == points[j].x){
-                    k = INT_MAX;   //避免0作为除数
+                if(points[i].x == points[j].x) {
+                    k = INT_MAX; //y坐标不同，x坐标相同，斜率无穷大
                 }
-                else{
+                else {
                     k = (float)(points[j].y - points[i].y)/(points[j].x - points[i].x);
                 }
-                mp[k]++;
+                mp[k]++; //统计相同斜率的点
             }
-            unordered_map<float, int>::iterator it = mp.begin();
-            for(; it != mp.end(); it++){
-                max = it->second + duplicate > max?it->second + duplicate : max;
+            unordered_map<float, int>::iterator it;
+            for(it = mp.begin(); it != mp.end(); it++) {
+                max_point = max(it->second + duplicate, max_point);
             }
         }
-        return max;
+        return max_point;
     }
 };
