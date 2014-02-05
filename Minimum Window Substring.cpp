b331fe25e87d6b23783955d7703f76e298e6ed50
Jacky Chen <jingchen@cmu.edu>
2014-02-05 09:53:27 -0800

    .\Minimum Window Substring.cpp

--- Minimum Window Substring.cpp
+++ Minimum Window Substring.cpp
@@ -1,6 +1,9 @@
+//这题展示了怎么样即简单又易懂的写出一个好的算法来，
+//把独立的功能用函数封装出去，这样问题规模就会缩小
 class Solution {
 public:
-      bool valid(int num1[], int num2[]){
+	//判断S是否能包住T，即T中的字符个数要小于S中对应的字符个数
+	bool valid(int num1[], int num2[]){
         for(int i=0; i<256; i++){
             if(num1[i]<num2[i]){
                 return false;
@@ -9,7 +12,6 @@ public:
 		return true;
     }
     string minWindow(string S, string T) {
-        // Note: The Solution object is instantiated only once and is reused by each test case.
         int s_num[256], t_num[256], c_num[256];
         int lenS = S.length(), lenT = T.length();
         int head = 0, tail = 0, minLen = lenS, head_min=0;
@@ -17,10 +19,12 @@ public:
 		memset(t_num,0,sizeof(s_num));
 		memset(c_num,0,sizeof(s_num));
         if(lenS<lenT) return "";
+		//统计S中各字符的个数
         for(int i=0; i<lenS; i++){
             s_num[S[i]]++;
         }
-        for(int i=0; i<lenT; i++){
+		//统计T中各字符的个数
+        for(int i=0; i<lenT; i++) {
             t_num[T[i]]++;
         }
         if(!valid(s_num, t_num)){
@@ -29,18 +33,18 @@ public:
         c_num[S[0]]++;
         while(tail<lenS){
             //如果不能包住,tail右移
-            if(!valid(c_num, t_num) || head>tail){
+            if(!valid(c_num, t_num)) {
                 c_num[S[++tail]]++;
             }
             //如果全包住,head右移
             else{
-                if(tail-head+1 < minLen){
+                if(tail-head+1 < minLen) { //刷新最小window长度
                     minLen = tail-head+1;
                     head_min = head;
                 }
                 c_num[S[head++]]--;
             }
         }
-        return S.substr(head_min, minLen);
+        return S.substr(head_min, minLen); //返回最小window
     }
 };
--- Substring with Concatenation of All Words.cpp
+++ Substring with Concatenation of All Words.cpp
@@ -1,23 +1,22 @@
+//这题其实不难，题意是S中要出现L中所有单词连在一起的字符串起点（不按顺序）
+//所以，这题甚至不需要用DFS或者动规
 class Solution {
 public:
     vector<int> findSubstring(string S, vector<string> &L) {
-        // Note: The Solution object is instantiated only once.  
         map<string,int> words, cur;  
         vector<int> res; 
         int wordNum = L.size();  
         if(!wordNum) return res;
-		int wordLen = L[0].size();
-        if(S.size() < wordLen*wordNum)return res; 
+		int wordLen = L[0].size(); //字典中的单词长度都是一样的
+        if(S.size() < wordLen*wordNum) return res; //S长度不够
 
         for(int i = 0; i < wordNum; i++)  
-            words[L[i]]++;  //记录下每个单词出现的个数
+            words[L[i]]++;  //记录下字典中每个单词出现的个数(因为可能有重)
 
-        for(int i = 0; i <= (int)S.size()-wordLen*wordNum; i++)  
-        {  
+        for(int i = 0; i <= (int)S.size()-wordLen*wordNum; i++) {  
             cur.clear(); //用来存放当前的查找情况
             int j;  
-            for(j = 0; j < wordNum; j++)  
-            {  
+            for(j = 0; j < wordNum; j++) {  
                 string word = S.substr(i+j*wordLen, wordLen);  
                 if(words.find(word) == words.end())  //找不到
                     break;  
