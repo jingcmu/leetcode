0efd90c22f96321ea093cf981835f83eb7b50402
Jacky Chen <jingchen@cmu.edu>
2014-02-05 12:10:58 -0800

    .\Surrounded Regions.cpp

--- Search for a Range.cpp
+++ Search for a Range.cpp
@@ -16,7 +16,7 @@ public:
 
 /*
 方法二：
-自己实现lower_bound和upper_bound算法
+重新实现lower_bound和upper_bound算法
 */
 class Solution {
 public:
@@ -30,11 +30,11 @@ public:
 	}
 
 	template<typename ForwardIterator, typename T>
-	ForwardIterator lower_bound (ForwardIterator first, ForwardIterator last, T value) {
+	ForwardIterator lower_bound (ForwardIterator first, ForwardIterator last, T target) {
 		while (first != last) {
 			auto mid = next(first, distance(first, last) / 2);
-			if (value > *mid) first = ++mid;
-			else last = mid;
+			if (*mid < target) first = ++mid; 
+			else last = mid; //等于跟大于一样，都移动last
 		}
 		return first;
 	}
@@ -43,74 +43,9 @@ public:
 	ForwardIterator upper_bound (ForwardIterator first, ForwardIterator last, T value) {
 		while (first != last) {
 			auto mid = next(first, distance (first, last) / 2);
-			if (value >= *mid) first = ++mid; // 与lower_bound 仅此不同
+			if (*mid <= target) first = ++mid; // 与lower_bound 仅此不同，等于跟小于一样，都移动last
 			else last = mid;
 		}
 		return first;
 	}
-};
-
-/*
-我自己的方法：
-
-*/
-class Solution {
-public:
-    void searchRange(vector<int> &v, int A[], int cur, int n, int target){
-        for(int i=cur; i>=0; i--){
-            if(A[i] == target){
-                v[0] = i;
-            }
-            else{
-                break;
-            }
-        }
-        for(int i=cur; i<n; i++){
-            if(A[i] == target){
-                v[1] = i;
-            }
-            else{
-                break;
-            }
-        }
-        return;
-    }
-    vector<int> searchRange(int A[], int n, int target) {
-        // Note: The Solution object is instantiated only once and is reused by each test case.
-        vector<int> v;
-        int head = 0, tail = n-1, cur=(head+tail)>>1;
-        v.resize(2);
-        v[0] = v[1] = -1;
-        if(n==0) {
-            return v;
-        }
-        if(n==1 && A[0] == target){
-            v[0] = v[1] = 0;
-            return v;
-        }
-        while(head < tail){
-            if(cur == head || cur == tail){
-                if(A[head] == target){
-                    cur = head;
-                }
-                else if(A[tail] == target){
-                    cur = tail;
-                }
-                searchRange(v,A,cur,n,target);
-                return v;
-            }
-            if(A[cur] > target){
-                tail = cur;
-            }
-            else if(A[cur] < target){
-                head = cur;
-            }
-            else{
-                searchRange(v,A,cur,n,target);
-                return v;
-            }
-            cur = (head+tail)>>1;
-        }
-        return v;
-    }
 };
--- Surrounded Regions.cpp
+++ Surrounded Regions.cpp
@@ -1,9 +1,16 @@
+/*
+这题就是把所有被X围住的O换成X，O没有被X全围住的情况就是那一组O连接了边界，
+只要把连接了边界的O全部先换成*（或其他字符），然后把剩余的O全部换成*，
+最后再把*换回O即可
+在把连接了边界的O全部先换成*这一步，用BFS宽搜
+*/
 class Solution {
 public:
     void solve(vector<vector<char>> &board) {
         int height = board.size(), width;
         if(height == 0) return;
         width = board[0].size();
+		//以两个竖边上的O为起点进行宽搜替换
         for(int i=0; i<height; i++){
             if(board[i][0] == 'O'){
                 bfs(i, 0, board);
@@ -12,7 +19,7 @@ public:
                 bfs(i, width-1, board);
             }
         }
-        
+        //以上下两个横边上的O为起点进行宽搜替换
         for(int j=1; j<width-1; j++){
             if(board[0][j] == 'O'){
                 bfs(0, j, board);
@@ -21,7 +28,7 @@ public:
                 bfs(height-1, j, board);
             }
         }
-        
+        //把O换成X，*换成O
         for(int i=0; i<height; i++){
             for(int j=0; j<width; j++){
                 if(board[i][j] == 'O'){
@@ -41,9 +48,9 @@ private:
     
     void bfs(int x, int y, vector<vector<char>> &board){
         queue<position> q;
-        visit(x, y, board, q);
+        visit(x, y, board, q); //访问处于边界上的点(起点)
         while(!q.empty()) {
-            //fetch one position from queue
+            //从队列取出一个点，并且访问其上下左右四个点
             position cur = q.front(); 
             q.pop();
             visit(cur.x-1, cur.y, board, q);
@@ -52,7 +59,7 @@ private:
             visit(cur.x, cur.y+1, board, q);
         }
     }
-    
+    //访问坐标为(x,y)的节点，如果是O，则替换成*，并放入队列
     void visit(int x, int y, vector<vector<char>> &board, queue<position> &q){
         if(x<0 || y<0 || x>=board.size() || y>=board[0].size() || board[x][y] == 'X'){
             return;
