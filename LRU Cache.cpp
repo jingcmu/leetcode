85ff5c19b3f87c645cd54e6d0c23a48556c892b6
Jacky Chen <jingchen@cmu.edu>
2014-02-06 15:34:58 -0800

    .\LRU Cache.cpp

--- LRU Cache.cpp
+++ LRU Cache.cpp
@@ -1,3 +1,12 @@
+/*
+这一题主要考察对数据结构特性的了解
+get函数显然应该以O(1)的复杂度实现，那么只能用unordered_map
+而且 这里map里面存的是list的迭代器！！！
+get函数要做的事情是：在map里查找key，如果找不到就返回-1；
+如果找到了，就把当前访问的节点放到list最前面，返回当前值
+set函数需要经常删除、插入，所以需要一个链表，在stl中当然是list了
+set函数要做的事情是：
+*/
 struct CacheNode{
     int key;
     int value;
@@ -15,15 +24,18 @@ public:
         if(cacheMap.find(key) == cacheMap.end()){ //cacheMap[key] == 0
             return -1;
         }
-        else { //假如在map中找到了key: 1.把节点放到链表头, 2.更新map中的记录, 3.返回value
-            cacheList.splice(cacheList.begin(), cacheList, cacheMap[key]);
-            cacheMap[key] = cacheList.begin();
-            return cacheList.front().value;
+		//假如在map中找到了key: 1.把节点放到链表头, 2.更新map中的记录, 3.返回value
+        else { 
+			//把cacheList的cacheMap[key]位置的点插入到cacheList.begin()
+            cacheList.splice(cacheList.begin(), cacheList, cacheMap[key]); 
+            cacheMap[key] = cacheList.begin();//更新map中的记录
+            return cacheList.front().value; //返回value
         }
     }
     
     void set(int key, int value) {
-        if(cacheMap.find(key) == cacheMap.end()) {    //没有找到节点: 1.插入节点并更新list和map, 2.要检查是否已满
+		//没有找到节点: 1.插入节点并更新list和map, 2.要检查是否已满
+        if(cacheMap.find(key) == cacheMap.end()) {    
             if(cacheList.size() == capacity){ //已满, 删除末尾的节点
                 cacheMap.erase(cacheList.back().key); //map要先调整
                 cacheList.pop_back();   
@@ -41,5 +53,5 @@ public:
 private:
     int capacity;
     list<CacheNode> cacheList;
-    unordered_map<int, list<CacheNode>::iterator> cacheMap;  
+    unordered_map<int, list<CacheNode>::iterator> cacheMap;  //map里面存的是<key, 节点位置(迭代器)>
 };
