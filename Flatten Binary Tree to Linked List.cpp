23b951c8fdcf34b037ff0533162779ece3c5b0b4
Jacky Chen <jingchen@cmu.edu>
2014-02-11 00:00:14 -0800

    .\Flatten Binary Tree to Linked List.cpp

--- Flatten Binary Tree to Linked List.cpp
+++ Flatten Binary Tree to Linked List.cpp
@@ -1,3 +1,36 @@
+/*
+理论上这个也不是原地算法，但是空间是O(log n)
+比下面的算法好，help返回值是链表的最后一个节点
+为什么？
+*/
+class Solution {
+public:
+    void flatten(TreeNode *root) {
+        if(!root) return;
+        else help(root);
+    }
+    
+    TreeNode * help(TreeNode *cur) {
+        if(!cur) return NULL;
+        if(cur->right == NULL && cur->left == NULL) return cur;
+        TreeNode *left = cur->left;
+        TreeNode *right = cur->right;
+        cur->left = NULL; //左指针一律置为null
+        if(left) {
+            cur->right = left; //右指针先指向左孩子
+            cur = help(left);  //把左孩子捋顺，返回的是左孩子的最后一个节点
+        }
+        if(right) {
+            cur->right = right;//右指针再指向右孩子
+            cur = help(right); //再把右孩子捋顺，返回的是右孩子的最后一个节点
+        }
+        return cur;
+    }
+};
+
+
+//下面这个不是原地算法，因为空间用了O(n+log(n))，
+//原地算法是空间只用O(1)
 /**
  * Definition for binary tree
  * struct TreeNode {
