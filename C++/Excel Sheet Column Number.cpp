c67c6c61982b859a9708c3390d86536ce3dcabf0
JackyChen <jackychen@google.com>
2015-03-30 21:57:06 -0700

    Excel Sheet Column Number.cpp

---
+++ C++/Excel Sheet Column Number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int titleToNumber(string s) {
+        int len = s.length();
+        int res = 0;
+        for (int i = 0; i < len; ++i) {
+            res *= 26;
+            res += (s[i] - 'A' + 1);
+        }
+        return res;
+    }
+};
