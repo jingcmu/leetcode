cc4f9cf7b08b7f1acadfb5e5f553e73f8e673740
Jacky Chen <jingchen@cmu.edu>
2015-05-23 13:06:33 -0700

    Update Shortest Palindrome.cc

--- C++/Shortest Palindrome.cc
+++ C++/Shortest Palindrome.cc
@@ -5,18 +5,16 @@ public:
         if (!len) return s;
         for (int i = len - 1; i >= 0; --i) {
             bool is_palindrom = true;
-            if (s[i] == s[0]) {
-                for (int j = 0; j < i/2; ++j) {
-                    if (s[j] != s[i - j]) {
-                        is_palindrom = false;
-                        break;
-                    }
+            for (int j = 0; j <= i/2; ++j) {
+                if (s[j] != s[i - j]) {
+                    is_palindrom = false;
+                    break;
                 }
-                if (is_palindrom) {
-                    string add_string = s.substr(i + 1);
-                    reverse(add_string.begin(), add_string.end());
-                    return add_string + s;
-                }
+            }
+            if (is_palindrom) {
+                string add_string = s.substr(i + 1);
+                reverse(add_string.begin(), add_string.end());
+                return add_string + s;
             }
         }
     }
