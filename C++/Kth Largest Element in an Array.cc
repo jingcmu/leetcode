03042ead2b0bb69c05e6e8bc0b73f5c4511018bf
Jacky Chen <jingchen@cmu.edu>
2015-05-22 21:43:19 -0700

    Update Kth Largest Element in an Array.cc

--- C++/Kth Largest Element in an Array.cc
+++ C++/Kth Largest Element in an Array.cc
@@ -1,3 +1,5 @@
+
+// 最简单解法
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
@@ -5,3 +7,21 @@ public:
         return nums[k-1];
     }
 };
+
+// priority queue 解法
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (pq.size() < k)
+                pq.push(nums[i]);
+            else {
+                pq.push(nums[i]);
+                pq.pop();
+            }
+        }
+        return pq.top();
+    }
+};
+
