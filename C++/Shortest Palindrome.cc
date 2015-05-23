63ee84d61acecb1a6532555d51d0da87c6b13c36
Jacky Chen <jingchen@cmu.edu>
2015-05-23 12:58:58 -0700

    Create Shortest Palindrome.cc

---
+++ C++/Shortest Palindrome.cc
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string shortestPalindrome(string s) {
+        int len = s.size();
+        if (!len) return s;
+        for (int i = len - 1; i >= 0; --i) {
+            bool is_palindrom = true;
+            if (s[i] == s[0]) {
+                for (int j = 0; j < i/2; ++j) {
+                    if (s[j] != s[i - j]) {
+                        is_palindrom = false;
+                        break;
+                    }
+                }
+                if (is_palindrom) {
+                    string add_string = s.substr(i + 1);
+                    reverse(add_string.begin(), add_string.end());
+                    return add_string + s;
+                }
+            }
+        }
+    }
+};
