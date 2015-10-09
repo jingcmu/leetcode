adcc578150c58c270a280a74993e6fd4f0539528
Jacky Chen <jingchencmu@gmail.com>
2015-10-08 22:35:41 -0700

    Update Symmetric Tree.java

--- java/Symmetric Tree.java
+++ java/Symmetric Tree.java
@@ -1,1 +1,29 @@
 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        return isSymmetric(root.left, root.right);
+    }
+    public boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        } else if (left == null || right == null) {
+            return false;
+        } else if (left.val != right.val) {
+            return false;
+        } else {
+            return isSymmetric(left.left, right.right) &&
+                isSymmetric(left.right, right.left);
+        }
+        
+    }
+}
