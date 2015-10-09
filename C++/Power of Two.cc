213db6e3e50d594c70cceaec7340f202545c5a3c
Jacky Chen <jingchencmu@gmail.com>
2015-10-08 22:57:20 -0700

    Update Power of Two.cc

--- C++/Power of Two.cc
+++ C++/Power of Two.cc
@@ -1,1 +1,11 @@
 
+public class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if (n <= 0) return false;
+        if (n == 1) return true;
+        while (n != 1 && n % 2 == 0) {
+            n /= 2;
+        }
+        return n == 1;
+    }
+}
