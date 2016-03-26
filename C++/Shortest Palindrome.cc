d3dc06165098d4b1a5ea6e12d15757b38ba32484
Jacky Chen <jingchencmu@gmail.com>
2016-03-26 12:26:39 -0700

    Update Shortest Palindrome.cc

--- C++/Shortest Palindrome.cc
+++ C++/Shortest Palindrome.cc
@@ -1,21 +1,23 @@
-class Solution {
-public:
-    string shortestPalindrome(string s) {
-        int len = s.size();
-        if (!len) return s;
-        for (int i = len - 1; i >= 0; --i) {
-            bool is_palindrom = true;
-            for (int j = 0; j <= i/2; ++j) {
-                if (s[j] != s[i - j]) {
-                    is_palindrom = false;
-                    break;
-                }
-            }
-            if (is_palindrom) {
-                string add_string = s.substr(i + 1);
-                reverse(add_string.begin(), add_string.end());
-                return add_string + s;
-            }
-        }
-    }
-};
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+string shortestPalindrome(string s) {
+  string r = s;
+  reverse(r.begin(), r.end());
+  string t = s + "#" + r;
+  // KMP
+  vector<int> p(t.size(), 0);
+  for (int i = 1; i < (int)t.size(); ++i) {
+    int j = p[i - 1]; // 第i-1个位置不匹配要跳到的位置
+    while (j > 0 && t[i] != t[j]) j = p[j - 1]; // 如果还是不匹配，跳到的位置
+    p[i] = (j += t[i] == t[j]); // 如果t[i] == t[j], 则p[i]在j的基础上+1
+  }
+  return r.substr(0, s.size() - p[t.size() - 1]) + s;
+}
+
+// To execute C++, please define "int main()"
+int main() {
+  cout << shortestPalindrome("ababc") << endl;
+  return 0;
+}
