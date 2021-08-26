f6028d59ca241e23838890dc7f60a90fac37f5dd
Jacky <jingcmu@qq.com>
2021-08-26 15:31:32 +0800

    Change Invert Binary  Tree

--- C++/Invert Binary Tree.cpp
+++ C++/Invert Binary Tree.cpp
@@ -17,3 +17,16 @@ public:
         return root;
     }
 };
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (!root) return NULL;
+        else {
+            TreeNode *temp = invertTree(root->left);
+            root->left = invertTree(root->right);
+            root->right = temp;
+        }
+        return root;
+    }
+};
