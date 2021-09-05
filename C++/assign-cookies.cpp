bcee7219facb3c663ae0af799477ef530d9c3a6d
jingcmu <jingcmu@qq.com>
2021-09-05 15:30:54 +0800

    assign-cookies

---
+++ C++/assign-cookies.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        int i = 0, j = 0, ret = 0;
+        while(i < g.size() && j < s.size()) {
+            if (g[i] <= s[j]) {
+                i++;
+                j++;
+                ret++;
+            } else {
+                j++;
+            }
+        }
+        return ret;
+    }
+};
