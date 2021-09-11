69bfa40c68fd675902e37744c6f76190d2247cff
jingcmu <jingcmu@qq.com>
2021-09-11 20:34:27 +0800

    count-primes

--- C++/count-primes.cpp
+++ C++/count-primes.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int countPrimes(int n) {
         vector<bool> nums(n, true);
-        for (int i = 2; i < n; i++) {
-            int j = 2;
+        for (int i = 2; i * i < n; i++) {
+            int j = i;
             while (i * j < n) {
                 nums[i * j] = false;
-                j ++;
+                j++;
             }
         }
         int res = 0;
         for (int i = 2; i < n; i++) {
             if (nums[i]){
-                res ++;
+                res++;
             }
         }
         return res;
